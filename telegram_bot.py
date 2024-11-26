a60b351e903e16ad09b6533dfb4df75d348ea8d9
Fuyumi <44784500+Fuyucch1@users.noreply.github.com>
2024-11-26 20:57:46 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,7 +6,7 @@ from traceback import print_exc
 from asyncio import queues
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
-VER = "0.5.1"
+VER = "0.5.2"
 
 
 # verify if bot still running
